682381adc354e946d717e8d35f132e965a2819cb
Rehan Raihan <68391710+raihanuddin2002@users.noreply.github.com>
2021-10-19 05:15:20 +0600

    Single service

--- src/components/Home/Services/Services.js
+++ src/components/Home/Services/Services.js
@@ -5,7 +5,7 @@ import Service from '../Service/Service';
 const Services = () => {
     const [services, setServices] = useState([]);
     useEffect( () => {
-        fetch("services.json")
+        fetch("./services.json")
             .then(res => res.json())
             .then(data => setServices(data))
     },[])
--- src/components/ServiceDetails/ServiceDetails.js
+++ src/components/ServiceDetails/ServiceDetails.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import useAuth from '../../hooks/useAuth';
 
 const ServiceDetails = () => {
     const {id} = useParams();
-    console.log(id)
+
+    const [services,setServices] = useState([]);
+    const [singleService, setSingleService] = useState({});
+
+    useEffect( () => {
+        fetch("/services.json")
+            .then(res => res.json())
+            .then(data => setServices(data))
+    },[])
+
+    useEffect( () =>{
+        const singleService = services.find(service => service.id == id);
+        setSingleService(singleService);
+    },[services]);
+    
+    
     return (
-        <div>
-            Hi!! {id}
+        <div className="container my-5 py-5">
+           <div className="cardd">
+            <h5 className="card-header text-center display-4 bg-dark text-white">Service</h5>
+            <div className="">
+            <table className="table table-striped table-bordered border-top-0 fs-4">
+                <tbody>
+                    <tr>
+                    <th scope="row">Service no</th>
+                    <td>{singleService?.id}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Service Name</th>
+                    <td>{singleService?.title}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Description</th>
+                    <td>{singleService?.description}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Doctor name</th>
+                    <td>{singleService?.doctor_name}</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">Degree</th>
+                    <td>{singleService?.degree}</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+            </div>
         </div>
     );
 };
